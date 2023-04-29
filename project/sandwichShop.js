b81313c01b39772f94f5098c7838ff4c16bf5d04
Glaydenn <glaydendev@gmail.com>
2023-04-29 17:12:12 -0400

    Removed Commented out Code
    Accidentally left unnecessary comments. Removed them.

--- project/sandwichShop.js
+++ project/sandwichShop.js
@@ -1,26 +1,3 @@
-/*function menuItem(name, price, desc)  {
-	this.name = name;
-	this.price = price;
-	this.desc = desc;
-	
-	this.display = function() {
-		let listNode = document.createElement("li"); 
-		listNode.className = "menuItem";
-		let textNode = document.createTextNode(this.name + "     " + "$" + this.price + "     " + "     " + this.desc);
-		listNode.appendChild(textNode);
-		
-		let listNodeImage = document.createElement("img");
-		listNodeImage.className = this.name;
-		
-		listNodeImage.src = "images/" + this.name.replace(/\s+/g, '') + ".jpeg";
-		listNode.appendChild(listNodeImage);
-		
-		let menuList = document.getElementById("menu");
-		menuList.appendChild(listNode);
-
-	}
-}*/
-
 const menuItems = [];
 const itemPrices = [];
 
